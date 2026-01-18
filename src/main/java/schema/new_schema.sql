97583b3cb169a96500543b74ec8e51d92ec8b7cd
Baby055 <hei.sombiniaina@gmail.com>
2026-01-18 20:01:34 +0300

    2nd commit

--- src/main/java/schema/new_schema.sql
+++ src/main/java/schema/new_schema.sql
@@ -23,3 +23,16 @@ CREATE TABLE DishIngredient {
     quantity_required NUMERIC (10,2) NOT NULL;
     unit unit_type NOT NULL;
 }
+
+INSERT INTO DishIngredient (id, id_dish, id_ingredient, quantity_required, unit) VALUES
+(1, 1, 1, 0.20, 'KG'),
+(2, 1, 2, 0.15, 'KG'),
+(3, 2, 3, 1.00, 'KG'),
+(4, 4, 4, 0.30, 'KG'),
+(5, 4, 5, 0.20, 'KG')
+
+UPDATE Dish SET selling_price = 3500.00 WHERE id=1;
+UPDATE Dish SET selling_price = 12000.00 WHERE id=2;
+UPDATE Dish SET selling_price = NULL WHERE id=3;
+UPDATE Dish SET selling_price = 8000.00 WHERE id=4;
+UPDATE Dish SET selling_price = NULL WHERE id=5;
